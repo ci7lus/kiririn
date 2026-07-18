5a2955feca8f8e275a4c5c08fdf00b8e51aca6e2
ci7lus <7887955+ci7lus@users.noreply.github.com>
2026-07-18 17:27:11 +0900

    chore: bmlビルド周り調整

--- setup.sh
+++ setup.sh
@@ -9,31 +9,35 @@ VLCKIT_DEST="./Packages/VLCKit"
 FRAMEWORK_DEST="${VLCKIT_DEST}/VLCKit.xcframework"
 LICENSE_NOTICES_DEST="${VLCKIT_DEST}/VLCKitAssets/LicenseNotices"
 
-rm -rf "$FRAMEWORK_DEST"
-rm -rf "$LICENSE_NOTICES_DEST"
+if [ "${VLCKIT_CACHE_HIT:-}" = "true" ]; then
+  echo "VLCKit cache hit"
+else
+  rm -rf "$FRAMEWORK_DEST"
+  rm -rf "$LICENSE_NOTICES_DEST"
 
-echo "Downloading VLCKit (DMG)..."
-MOUNT_DIR=$(mktemp -d)
+  echo "Downloading VLCKit (DMG)..."
+  MOUNT_DIR=$(mktemp -d)
 
-TEMP_DMG="tmp.dmg"
-curl -sL "$VLCKIT_URL" -o "$TEMP_DMG"
-echo "Mounting DMG...: $MOUNT_DIR"
-hdiutil attach "$TEMP_DMG" -mountpoint "$MOUNT_DIR" -nobrowse -quiet
+  TEMP_DMG="tmp.dmg"
+  curl -sL "$VLCKIT_URL" -o "$TEMP_DMG"
+  echo "Mounting DMG...: $MOUNT_DIR"
+  hdiutil attach "$TEMP_DMG" -mountpoint "$MOUNT_DIR" -nobrowse -quiet
 
-echo "Extracting libraries from DMG..."
-cp -R "$MOUNT_DIR/VLCKit.xcframework" "$FRAMEWORK_DEST"
+  echo "Extracting libraries from DMG..."
+  cp -R "$MOUNT_DIR/VLCKit.xcframework" "$FRAMEWORK_DEST"
 
-echo "Extracting VLCKit license notices from DMG..."
-mkdir -p "$LICENSE_NOTICES_DEST"
-cp "$MOUNT_DIR/License Notices/licenses/"*.txt "$LICENSE_NOTICES_DEST"
+  echo "Extracting VLCKit license notices from DMG..."
+  mkdir -p "$LICENSE_NOTICES_DEST"
+  cp "$MOUNT_DIR/License Notices/licenses/"*.txt "$LICENSE_NOTICES_DEST"
 
-hdiutil detach "$MOUNT_DIR" -quiet
-rm -rf "$TEMP_DMG" "$MOUNT_DIR"
+  hdiutil detach "$MOUNT_DIR" -quiet
+  rm -rf "$TEMP_DMG" "$MOUNT_DIR"
 
-curl -sL "https://raw.githubusercontent.com/neneka/vlckit/refs/tags/$REVISION/share/hrtfs/dodeca_and_7channel_3DSL_HRTF.sofa" -o "$VLCKIT_DEST/VLCKitAssets/dodeca_and_7channel_3DSL_HRTF.sofa"
+  curl -sL "https://raw.githubusercontent.com/neneka/vlckit/refs/tags/$REVISION/share/hrtfs/dodeca_and_7channel_3DSL_HRTF.sofa" -o "$VLCKIT_DEST/VLCKitAssets/dodeca_and_7channel_3DSL_HRTF.sofa"
 
-# Rounded M+ 1m WadaLab mix ARIB
-curl -sL "https://github.com/vivid-lapin/rounded-mplus-wadalab-mix/releases/download/202606272034/rounded-mplus-1m-wadalab-comp-arib.ttf" -o "kiririn/rounded-mplus-1m-wadalab-comp-arib.ttf"
+  # Rounded M+ 1m WadaLab mix ARIB
+  curl -sL "https://github.com/vivid-lapin/rounded-mplus-wadalab-mix/releases/download/202606272034/rounded-mplus-1m-wadalab-comp-arib.ttf" -o "kiririn/rounded-mplus-1m-wadalab-comp-arib.ttf"
+fi
 
 # BML (データ放送) Webバンドル
 echo "Building BML web bundle..."
